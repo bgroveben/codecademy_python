17f10a5366e7b60b0165e80ee77c4ea2db97d460
bgrove1974 <bgroveben@gmail.com>
2015-08-06 23:23:16 -0700

    Functions

--- some_functions.py
+++ some_functions.py
@@ -10,3 +10,29 @@ def by_three(number):
 print cube(9)
 print by_three(15)
 print by_three(7)
+print
+
+
+def shut_down(s):
+    if s == "yes":
+        return "Shutting down"
+    elif s == "no":
+        return "Shutdown aborted"
+    else:
+        return "Sorry"
+
+print shut_down("yes")
+print shut_down("no")
+print shut_down("spam")
+print
+
+
+def distance_from_zero(n):
+    if type(n) == int or type(n) == float:
+        return abs(n)
+    else:
+        return "Nope"
+
+print distance_from_zero(-42)
+print distance_from_zero(-4.2)
+print distance_from_zero("spam")
