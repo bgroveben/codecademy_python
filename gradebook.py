744ff2c6f273479d59c528e40662d635fe598c12
bgrove1974 <bgroveben@gmail.com>
2015-08-09 01:12:21 -0700

    Calculate average scores and weighted grade average

--- gradebook.py
+++ gradebook.py
@@ -1,6 +1,5 @@
 # Time to teach your own class.
 # Make a gradebook for all of your students.
-
 lloyd = {
     "name": "Lloyd",
     "homework": [90.0, 97.0, 75.0, 92.0],
@@ -20,9 +19,30 @@ tyler = {
     "tests": [100.0, 100.0]
 }
 
+# Create a list called students that contains lloyd, alice, and tyler.
 students = [lloyd, alice, tyler]
+# For each student in your students list, print out that student's data.
 for student in students:
     print student["name"]
     print student["homework"]
     print student["quizzes"]
     print student["tests"]
+
+# Write a function average that takes a list of numbers and returns the average.
+def average(numbers):
+    total = sum(numbers)
+    total = float(total)
+    return total / len(numbers)
+# print average(tyler["tests"])  # => 100.0
+# print average(alice["homework"])  # => 97.5
+
+# Write a function called get_average that takes a student dictionary as input and returns his/her weighted average.
+def get_average(student):
+    homework = average(student["homework"])
+    quizzes = average(student["quizzes"])
+    tests = average(student["tests"])
+    return (0.1 * homework) + (0.3 * quizzes) + (0.6 * tests)
+
+print get_average(tyler)  # => 79.9
+print get_average(alice)  # => 91.15
+print get_average(lloyd)  # => 80.55
